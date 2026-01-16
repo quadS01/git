6d8f4db337bfdcf768f4c39df3fd6108cb1ce812
yzarhoun <hansel.wh@proton.me>
2026-01-16 13:02:00 +0100

    Updating hello.sh

--- lib/hello.sh
+++ lib/hello.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Default is World
-# Author: Jim Weirich
-# E-mail: jim@zone01oujda.ma 
-name=${1:-"World"}
+source lib/greeter.sh
 
-echo "Hello, $name"
+name="$1"
+if [ -z "$name" ]; then
+    name="World"
+fi
+
+Greeter "$name"
